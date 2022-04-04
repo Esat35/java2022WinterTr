478f0a5d8b3fd1edacb23008773f56916105f1e6
Esat35 <dugindugin35@gmail.com>
2022-04-04 21:56:31 +0300

    04.04.22

---
+++ src/day34_deneme/DenemeChild.java
@@ -0,0 +1,8 @@
+package day34_deneme;
+import day34_accessModifier_encapsulation.C01;
+public class DenemeChild extends C01 {
+    public static void main(String[] args) {
+        System.out.println(halkaAcikSayi);
+        halkaAcikSayi=30;
+    }
+}
---
+++ src/day35_inheritance/Personel.java
@@ -0,0 +1,6 @@
+package day35_inheritance;
+public class Personel {
+    public String isim;
+    protected String soyisim;
+    protected String adres;
+}

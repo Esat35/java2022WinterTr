92da99fc6718a48b38da735c5f669dc66ff615d5
Esat35 <dugindugin35@gmail.com>
2022-04-06 17:55:51 +0300

    06.04.22

---
+++ src/day36_inheritance/Parent.java
@@ -0,0 +1,7 @@
+package day36_inheritance;
+public class Parent {
+   protected Parent(){
+        System.out.println("Parent cons. calisti");
+    }
+    protected int sayi;
+}
---
+++ src/day37_inheritance/Child.java
@@ -0,0 +1,9 @@
+package day37_inheritance;
+
+import day36_inheritance.Parent;
+
+public class Child extends Parent {
+    Child(){
+        super();
+    }
+}

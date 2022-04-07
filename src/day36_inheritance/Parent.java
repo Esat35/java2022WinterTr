c9fa1b4937897cb31661cb27dd9e114996c56737
Esat35 <dugindugin35@gmail.com>
2022-04-08 00:17:20 +0300

    07.04.22

--- src/day36_inheritance/Parent.java
+++ src/day36_inheritance/Parent.java
@@ -1,7 +1,10 @@
 package day36_inheritance;
 public class Parent {
-   protected Parent(){
-        System.out.println("Parent cons. calisti");
+    protected Parent(){
+        System.out.println("Parent parametresiz cons. calisti");
+    }
+    protected Parent(int sayi3, int sayi4){
+        System.out.println("Parent 2 parametreli cons");
     }
     protected int sayi;
 }

40c67ef622f8b839b2604ff3333240e4d5704f37
Esat35 <dugindugin35@gmail.com>
2022-04-12 11:26:31 +0300

    12.04.2022

--- src/day40_overriding_polymorphism/C02.java
+++ src/day40_overriding_polymorphism/C02.java
@@ -29,11 +29,9 @@ public class C02 extends C01{
         C02 obj = new C02();
         obj.method1(); // parent method1
         obj.method2(); // child method2
-
         C01 obj2 = new C02();
         obj2.method1(); // parent method1
         obj2.method2(); // child method2
-
         C01 obj3 = new C01();
         obj3.method1(); // parent method1
         obj3.method2(); // parent method2
--- src/day40_overriding_polymorphism/C05_Exception.java
+++ src/day40_overriding_polymorphism/C05_Exception.java
@@ -3,11 +3,9 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 public class C05_Exception {
     public static void main(String[] args) {
-
         // Kullanicidan istedigi kadar sayiyi alip toplayan bir program yaziniz
         // toplam 500'u gecerse programi bitirsin veya
         // kullanici bitirmek istediginde Q'ya basmalidir
-
         Scanner scan = new Scanner(System.in);
         int toplam=0;
         do{
@@ -16,7 +14,6 @@ public class C05_Exception {
             try {
                 sayi = scan.nextInt();
             } catch (InputMismatchException e) {
-
             }
             toplam+=sayi;
         }while (toplam<500);
